54ae8435fd6e29082139bda03e106a4073031b7c
Csávás Gergely <csavas.g95@gmail.com>
2024-03-13 12:12:50 +0100

    Dinamikus API adatok kiírása az oldalra / Adat változók készítése, refaktorálása / CSS készítése az adatok megjelenítéséhez

--- JitsiTestApp/src/app/app.component.ts
+++ JitsiTestApp/src/app/app.component.ts
@@ -17,36 +17,67 @@ export class AppComponent implements AfterViewInit {
 
   api: any = null;
 
+  envType: string = 'N/A';
+  region: string = 'N/A';
+  shard: string = 'N/A';
+  displayName: string = '';
+
+  browserSupported: boolean = false;
+  mouseIsInFrame: boolean = false;
+  cameraIsOn: boolean = false;
+  displayNameIsChanged: boolean = false;
+
   ngAfterViewInit(): void {
     const options = {
       roomName: 'MedicalScanJitsi',
       width: 900,
       height: 600,
       parentNode: document.getElementById('meet'),
-      lang: 'hu'
+      lang: 'hu',
+      userInfo: {
+        displayName: 'Jitsi User',
+        email: 'example@email.com'
+      }
     }
     this.api = new JitsiMeetExternalAPI(this.domain, options);
 
     this.api!.addListener('browserSupport', (status: any) => {
-      console.log(status);
+      this.browserSupported = status;
     });
 
     this.api!.addListener('mouseEnter', (event: any) => {
-      console.log(event);
+      this.mouseIsInFrame = true;
+    });
+
+    this.api!.addListener('mouseLeave', (event: any) => {
+      this.mouseIsInFrame = false;
     });
 
     this.api!.addListener('displayNameChange', (event: any) => {
-      console.log(event);
+      if (!this.displayNameIsChanged) {
+        this.displayNameIsChanged = true;
+      }
+      this.displayName = event.displayname;
     });
 
-    this.api!.getDeploymentInfo().then((deploymentInfo: any) => {
-      console.log(deploymentInfo);
+    this.api!.addListener('videoMuteStatusChanged', (event: any) => {
+      this.cameraIsOn = !event.muted;
     });
   }
 
   toggleVideoDevice() {
-    if(this.api) {
+    if (this.api) {
       this.api.executeCommand('toggleVideo');
+    }
+  }
+
+  requestDeploymentInfo() {
+    if (this.api) {
+      this.api.getDeploymentInfo().then((deploymentInfo: any) => {
+        this.envType = deploymentInfo.envType;
+        this.region = deploymentInfo.region;
+        this.shard = deploymentInfo.shard;
+      });
     }
   }
 }
