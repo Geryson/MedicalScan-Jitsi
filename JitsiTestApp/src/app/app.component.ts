62138fd75ce80f937b857f6a165ac9685e2f282b
Csávás Gergely <csavas.g95@gmail.com>
2024-03-13 10:31:29 +0100

    JitsiMeetExternalAPI működik

--- JitsiTestApp/src/app/app.component.ts
+++ JitsiTestApp/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+declare var JitsiMeetExternalAPI: any;
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'JitsiTestApp';
+
+  domain = 'meet.jit.si';
+
+  ngAfterViewInit(): void {
+    const options = {
+      roomName: 'MedicalScanJitsi',
+      width: 700,
+      height: 700,
+      parentNode: document.getElementById('meet'),
+      lang: 'hu'
+    }
+    const api = new JitsiMeetExternalAPI(this.domain, options);
+  }
 }
